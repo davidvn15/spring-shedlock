cd8a087895003bbfad25e49962c56b783d645abb
canhnd@newwave.com.vn <canhnd@newwave.com.vn>
2025-03-05 16:02:02 +0700

    add tasks

--- src/main/java/com/davidnguyen/shedlock/config/MyScheduledTask.java
+++ src/main/java/com/davidnguyen/shedlock/config/MyScheduledTask.java
@@ -9,9 +9,21 @@ import java.time.LocalDateTime;
 @Component
 public class MyScheduledTask {
     @Scheduled(cron = "0 */1 * * * *")
-    @SchedulerLock(name = "MyScheduledTaskLock", lockAtMostFor = "1m", lockAtLeastFor = "1m")
-    public void performTask() {
-        System.out.println("Executing scheduled task at: " + LocalDateTime.now());
+    @SchedulerLock(name = "MyScheduledTaskLock1", lockAtMostFor = "1m", lockAtLeastFor = "1m")
+    public void performTask1() {
+        System.out.println("Executing scheduled task 1 at: " + LocalDateTime.now());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Task completed at: " + LocalDateTime.now());
+    }
+
+    @Scheduled(cron = "0 */1 * * * *")
+    @SchedulerLock(name = "MyScheduledTaskLock2", lockAtMostFor = "20s", lockAtLeastFor = "20s")
+    public void performTask2() {
+        System.out.println("Executing scheduled task 2 at: " + LocalDateTime.now());
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
